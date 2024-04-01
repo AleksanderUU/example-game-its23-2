5e76e74e9a058ad3978b8a15633bc87f04abb364
aleksander <aleksander.uibu@voco.ee>
2024-04-01 21:16:50 +0300

    As a player I can see the game window (Delivers #187347079]

--- game.py
+++ game.py
@@ -2,22 +2,28 @@
 
 # import and initialize the pygame library
 import pygame
-pygame.init()
+from settings import Settings
 
-# set up the drawing window
-screen = pygame.display.set_mode([800, 500])
+def run_game():
+    pygame.init()
+    gm_settings = Settings()
 
-# run until the user asks to quit
-running = True
-while running:
-    
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    # Flip the Display
-    pygame.display.flip()
+    # Set up the drawing window
+    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+    pygame.display.set_caption(gm_settings.caption)
 
-# Done! Time to quit.
-pygame.quit()
+    # run until the user asks to quit
+    running = True
+    while running:
+        screen.fill(gm_settings.bg_color)
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        # Flip the Display
+        pygame.display.flip()
+    # Done! Time to quit.
+    pygame.quit()
+
+run_game()
